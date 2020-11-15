7a6ae45466ddda8f17391da4ccbb1c83f840b2c7
guitheindl <guitheindl@gmail.com>
2020-11-15 01:26:22 -0300

    implementando composicao e agregacao

--- backend/model.py
+++ backend/model.py
@@ -2,15 +2,17 @@
 from config import *
 
 class Moto(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     modelo = db.Column(db.String(254))
     cilindradas = db.Column(db.String(254))
     motor = db.Column(db.String(254))
 
+    oficina = db.relationship("Oficina", back_populates="moto")
+
+
     def __str__(self):
-        return str(self.id)+") "+ self.modelo + ", " +\
-            self.cilindradas + ", " + self.motor
+        return f"{self.id}. {self.modelo}; {self.cilindradas}; {self.motor}"
+
 
     def json(self):
         return {
@@ -20,6 +22,55 @@ class Moto(db.Model):
             "motor": self.motor
         }
 
+
+class Mecanico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    cargo = db.Column(db.String(254))
+
+    oficina = db.relationship("Oficina", back_populates="mecanico")
+
+
+    def __str__(self):
+        return f"{self.id}. {self.nome}; {self.cargo}"
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "cargo": self.cargo
+        }
+
+
+class Oficina(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    endereco = db.Column(db.String(254))
+
+    mecanico_id = db.Column(db.Integer, db.ForeignKey(Mecanico.id), nullable=False)
+    mecanico = db.relationship("Mecanico", back_populates="oficina")
+
+    moto_id = db.Column(db.Integer, db.ForeignKey(Moto.id), nullable=False)
+    moto = db.relationship("Moto", back_populates="oficina")
+
+
+    def __str__(self):
+        return f"{self.id}. {self.nome}; {self.endereco}; {self.mecanico}; {self.moto}"
+
+
+    def json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "endereco": self.endereco,
+            "mecanico_id": self.mecanico_id,
+            "mecanico": self.mecanico,
+            "moto_id": self.moto_id,
+            "moto": self.moto
+        }
+
+
 if __name__ == "__main__":
 
     if os.path.exists(arquivobd):
@@ -27,14 +78,26 @@ if __name__ == "__main__":
 
     db.create_all()
 
-    moto1 = Moto(modelo = "xt 660", cilindradas = "550cc", 
-        motor = "4t")
-    moto2 = Moto(modelo = "cb 500", cilindradas = "470cc", 
-        motor = "4t")        
-    
+    moto1 = Moto(modelo="xt 660", cilindradas="550cc", motor="4t")
     db.session.add(moto1)
-    db.session.add(moto2)
+
+    mecanico1 = Mecanico(nome="Guilherme G. Theindl", cargo="Dono")
+    db.session.add(mecanico1)
+
+    oficina1 = Oficina(nome="IFCar", endereco="Rua dos Bobos, 0",
+                       moto=moto1, mecanico=mecanico1)
+
+    db.session.add(oficina1)
+
+
     db.session.commit()
-    
-    print(moto2)
-    print(moto2.json())
+
+    linha = '------------------------------------------------------------------'
+    print(moto1)
+    print(moto1.json())
+    print(linha)
+    print(mecanico1)
+    print(mecanico1.json())
+    print(linha)
+    print(oficina1)
+    print(oficina1.json())
